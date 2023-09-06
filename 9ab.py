ee92296b4859c4bfe575abfdfcf4c93eed513896
Revaldfernandes <131395182+Revaldfernandes@users.noreply.github.com>
2023-09-06 05:33:46 +0530

    Update 9ab.py

--- 9ab.py
+++ 9ab.py
@@ -31,4 +31,38 @@ print("reading Row3",sheet['A3'].value)
 cells=sheet['A1':'B3']
 for c1,c2 in cells:
     print(c1.value,c2.value)
+
+
+//////////
+
+
+import request
+import os
+from bs4 import BeautifulSoup
+url="https=//xkcd.com/1/"
+if not os.path.exists('xkcd_comics'):
+    os.makeclirs('xkcd_comics')
+    while true:
+        res=requests.get(url)
+        res.raise_for_status()
+        soup=BeautifulSoup(res.txt,'html,parser')
+        comic_elem=soup.select('#comiccimg')
+        if comic elem==[]:
+            print("could not find comic image")
+        else:
+            comic.url='https:'+comic_elen[0].get('src')
+            print(f'Downloading{comic_url}..')
+            res=requests.get(comic_url)
+            res.raise_for_staus()
+            image_file=open(os.path.join("xkcd_comics",os.path.basename(comic_url)),'wh')
+            for chunk in res.iter_content(100000):
+                image_file.write(chunk)
+                image_file.close()
+                prev_link=soup.select('a[rel="prev"]')[0]
+                if not prev_link:
+                    break
+                    url='https://xkcd.com'+prev_link.get('href')
+                    print("all comics clown loaded")
+
+
     
